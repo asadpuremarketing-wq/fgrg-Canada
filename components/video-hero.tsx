066119cb58cd06043ffb7cdfc6afb0e795e0efac
asad <asadsaif400@gmail.com>
2026-04-23 12:48:34 -0400

    fix: remove duplicate autoPlay attribute in VideoHero

--- components/video-hero.tsx
+++ components/video-hero.tsx
@@ -1,4 +1,4 @@
-﻿"use client";
+"use client";
 
 import Image from "next/image";
 import Link from "next/link";
@@ -67,7 +67,6 @@ export function VideoHero({
 
         {/* Video overlays the photo when loaded */}
         <video
-          autoPlay
           muted
           loop
           playsInline
